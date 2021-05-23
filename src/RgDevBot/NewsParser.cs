f0b29c6902c118552cc118af27286399a8469e04
artem.sedykh <anavrin72@gmail.com>
2021-05-23 20:42:49 +0300

    add uk-kc news

--- src/RgDevBot/NewsParser.cs
+++ src/RgDevBot/NewsParser.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using AngleSharp.Common;
+using AngleSharp.Html.Parser;
 using Newtonsoft.Json;
 using RgDevBot.Config;
 using RgDevBot.ObjectModel;
@@ -21,27 +24,34 @@ namespace RgDevBot
 
         public void Parse()
         {
-            var content = Get();
+            var content = Get(20);
 
             var root = JsonConvert.DeserializeObject<Root>(content);
-            var latestNews = root.data.allPosts.edges.OrderByDescending(e => e.node.date).ToArray();
 
-            Console.WriteLine($"[{DateTime.Now}] Получено {latestNews.Length} новостей. Самая новая: {latestNews?.FirstOrDefault()?.node.title}.");
+            var absrealtyNews = root.data.allPosts.edges.Select(e=>e.node).OrderByDescending(e => e.date).ToArray();
+
+            Console.WriteLine($"[{DateTime.Now}] Получено {absrealtyNews.Length} новостей(absrealty). Самая новая: {absrealtyNews.FirstOrDefault()?.title}.");
+
+            var ukkcNews = GetUKKCNews();
+
+            Console.WriteLine($"[{DateTime.Now}] Получено {ukkcNews.Length} новостей(ukkc). Самая новая: {ukkcNews.FirstOrDefault()?.title}.");
+
+            var latestNews = absrealtyNews.Concat(ukkcNews).ToArray();
 
             foreach (var post in latestNews)
             {
-                if (_config.ConfigValues.Contains(post.node.id))
+                if (_config.ConfigValues.Contains(post.id))
                 {
                     continue;
                 }
 
                 try
                 {
-                    var text = $"{post.node.title}:\r\nhttps://www.absrealty.ru/news/{post.node.slug}/";
+                    var text = post.GetText();
                     Console.WriteLine(text);
                     _bot.SendMessage(text);
 
-                    _config.ConfigValues.Add(post.node.id);
+                    _config.ConfigValues.Add(post.id);
                     _config.Save();
                 }
                 catch (Exception ex)
@@ -51,14 +61,14 @@ namespace RgDevBot
             }
         }
 
-        public string Get(int lastNews = 20)
+        public string Get(int top = 20)
         {
-            var totalPages = GetTotalPages();
+            var total = GetTotalPages();
 
-            if ((totalPages - lastNews) < 0)
-                lastNews = totalPages;
+            if ((total - top) < 0)
+                top = total;
 
-            var after = Base64Encode($"arrayconnection:${totalPages - lastNews + 1}");
+            var after = Base64Encode($"arrayconnection:${total - top}");
 
             var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://www.absrealty.ru/graphql/");
             httpWebRequest.ContentType = "application/json";
@@ -70,7 +80,7 @@ namespace RgDevBot
                     "{\"query\":\"query allPosts($first: Int, $after: String, $isSpecial: Boolean, $project: String, $category: String, $year: String) {\\n  allPosts(first: $first, after: $after, isSpecial: $isSpecial, project: $project, category: $category, year: $year) " +
                     "{\\n    pageInfo {\\n      endCursor\\n      hasNextPage\\n    }\\n    totalCount\\n    edges {\\n      node {\\n        id\\n        slug\\n        date\\n        title\\n        shortDescription\\n       " +
                     " projects {\\n          id\\n          title\\n          slug\\n          outerSite\\n        }\\n        imagePageNewsDisplay\\n        imagePageNewsPreview\\n        imageDisplay\\n        imagePreview\\n      }\\n    }\\n  }\\n}" +
-                    "\\n\",\"variables\":{\"project\":\"peredelkino-blizhnee\",\"first\":" + lastNews + ",\"after\":\"" + after + "\"}}";
+                    "\\n\",\"variables\":{\"project\":\"peredelkino-blizhnee\",\"first\":" + top + ",\"after\":\"" + after + "\"}}";
 
                 streamWriter.Write(json);
             }
@@ -104,7 +114,9 @@ namespace RgDevBot
 
                 var root = JsonConvert.DeserializeObject<Root>(json);
                 var totalPages = (root?.data.allPosts.totalCount).GetValueOrDefault();
+
                 Console.WriteLine($"[{DateTime.Now}] Всего {totalPages} новостей.");
+
                 return totalPages;
             }
         }
@@ -113,6 +125,55 @@ namespace RgDevBot
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return Convert.ToBase64String(plainTextBytes);
+        }
+
+        private static Node[] GetUKKCNews()
+        {
+            var result = new List<Node>();
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://www.uk-kc.ru/peredelkino/?news_count=50");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using var streamReader = new StreamReader(httpResponse.GetResponseStream());
+
+            var html = streamReader.ReadToEnd();
+
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(html);
+
+            var news = document.QuerySelectorAll(".content-padding .news.clearfix");
+
+            foreach (var element in news)
+            {
+                if (string.IsNullOrWhiteSpace(element.Id))
+                {
+                    continue;
+                }
+
+                foreach (var aElement in element.QuerySelectorAll("a"))
+                {
+                    var h2Elements = aElement.QuerySelectorAll("h2");
+
+                    if (h2Elements.Length <= 0) continue;
+
+                    var title = h2Elements.First().InnerHtml;
+                    var href = aElement.Attributes["href"].Value;
+
+                    result.Add(new Node
+                    {
+                        id = element.Id,
+                        title = title,
+                        slug = href,
+                        type = NewsSource.uk_kc
+                    });
+
+                    break;
+                }
+            }
+
+            return result.ToArray();
         }
     }
 }
--- src/RgDevBot/ObjectModel/Root.cs
+++ src/RgDevBot/ObjectModel/Root.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace RgDevBot.ObjectModel
 {
+    public enum NewsSource { absrealty = 0,  uk_kc = 1 }
+
+
     public class PageInfo
     {
         public string endCursor { get; set; }
@@ -19,16 +22,42 @@ namespace RgDevBot.ObjectModel
 
     public class Node
     {
+        private static Uri UKKCBaseUri = new Uri("https://www.uk-kc.ru/peredelkino/");
+
         public string id { get; set; }
         public string slug { get; set; }
         public DateTime date { get; set; }
         public string title { get; set; }
-        public string shortDescription { get; set; }
-        public List<Project> projects { get; set; }
-        public object imagePageNewsDisplay { get; set; }
-        public object imagePageNewsPreview { get; set; }
-        public object imageDisplay { get; set; }
-        public object imagePreview { get; set; }
+        public NewsSource type { get; set; }
+
+        public string GetUrl()
+        {
+            switch (type)
+            {
+                case NewsSource.absrealty:
+                    return $"https://www.absrealty.ru/news/{slug}/";
+                case NewsSource.uk_kc:
+                {
+                    if (Uri.TryCreate(slug, UriKind.Absolute, out _)) 
+                        return slug;
+
+                    var url = (new Uri(UKKCBaseUri, slug)).ToString();
+                    if (url.EndsWith("/") == false)
+                        url += "/";
+
+                    return url;
+                }
+                default:
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        public string GetText()
+        {
+            return $"{type.ToString()}: {title}:\r\n{GetUrl()}";
+        }
     }
 
     public class Edge
